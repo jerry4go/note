40bbc43cf5aba2f59769059bf3f809acd979f563
jerry4go <43691453+jerry4go@users.noreply.github.com>
2026-06-20 12:39:31 +0800

    auto sync 周六 2026/06/20 12:39:31.38

--- auto_sync_github.bat
+++ auto_sync_github.bat
@@ -1,31 +1,10 @@
 @echo off
 chcp 65001 >nul
 set "work_dir=D:\02-MarkDown文档"
-cd /d "%work_dir%" || (
-    echo 错误：目录不存在 %work_dir%
-    pause
-    exit /b 1
-)
-
-echo 拉取GitHub远程更新
+cd /d "%work_dir%" || exit /b 1
 git pull origin main
-
-echo 扫描全部文件变更
 git add .
-
-:: 判断是否存在未提交改动
 git status --porcelain
-if "%errorlevel%"=="0" (
-    echo 检测到文件变更，开始提交推送
-) else (
-    echo 未检测到文件修改，同步结束
-    pause
-    exit
-)
-
+if errorlevel 1 exit
 git commit -m "auto sync %date% %time%"
-echo 推送文件到GitHub
 git push origin main
-echo ======================
-echo 全部同步完成！
-pause
